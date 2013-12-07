82f6da1aaf693a391ce5066993172edcde7b4f0b
Kim, Jang-hwan <janghwan@gmail.com>
2013-12-08 01:52:12 +0900

    add more powers to run_tests
    1. can run any kind of names:
    
    		python run_tests.py lib/google-app-engine tests.test_handlers
    		python run_tests.py lib/google-app-engine tests.test_handlers.WikiPageHandlerTest
    		python run_tests.py lib/google-app-engine tests.test_handlers.WikiPageHandlerTest.test_post_new_page_should_fail_if_user_is_none
    
    2. can run any combination of test suites:
    
    		python run_tests.py lib/google-app-engine
    			tests.test_handlers.RevisionTest
    			tests.test_handlers.WikiPageHandlerTest.test_post_new_page_should_fail_if_user_is_none

--- run_tests.py
+++ run_tests.py
@@ -10,33 +10,39 @@ SDK_PATH    Path to the SDK installation
 TEST_PATH   Path to package containing test modules"""
 
 
-def main(sdk_path, test_path):
+def main(sdk_path, test_paths):
     sys.path.insert(0, sdk_path)
     import dev_appserver
     dev_appserver.fix_sys_path()
-    # tests/ directory
-    if os.path.isdir(test_path):
-        suite = unittest2.loader.TestLoader().discover(test_path)
-    # test_file.py
-    elif os.path.isfile(test_path):
-        test_path, test_file = test_path.rsplit(os.path.sep, 1)
-        suite = unittest2.loader.TestLoader().discover(test_path, test_file)
-    # tests.module.TestCase
-    else:
-        module_name, class_name = test_path.rsplit('.', 1)
-        module = __import__(module_name, fromlist=[module_name])
-        testcase = getattr(module, class_name)
-        suite = unittest2.loader.TestLoader().loadTestsFromTestCase(testcase)
-    unittest2.TextTestRunner(verbosity=2).run(suite)
+
+    suites = unittest2.TestSuite()
+    for test_path in test_paths:
+        # tests/ directory
+        if os.path.isdir(test_path):
+            suite = unittest2.loader.TestLoader().discover(test_path)
+        # test_file.py
+        elif os.path.isfile(test_path):
+            test_path, test_file = test_path.rsplit(os.path.sep, 1)
+            suite = unittest2.loader.TestLoader().discover(test_path, test_file)
+        # tests.module.TestCase
+        elif '/' not in test_path and '.' in test_path:
+            #module_name, class_name = test_path.rsplit('.', 1)
+            #module = __import__(module_name, fromlist=[module_name])
+            #testcase = getattr(module, class_name)
+            #suite = unittest2.loader.TestLoader().loadTestsFromTestCase(testcase)
+            suite = unittest2.loader.TestLoader().loadTestsFromName(test_path)
+        suites.addTest(suite)
+    unittest2.TextTestRunner(verbosity=2).run(suites)
 
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(USAGE)
     options, args = parser.parse_args()
-    if len(args) != 2:
-        print 'Error: Exactly 2 arguments required.'
+    if len(args) < 2:
+        print 'Error: At least 2 arguments required.'
         parser.print_help()
         sys.exit(1)
     SDK_PATH = args[0]
-    TEST_PATH = args[1]
-    main(SDK_PATH, TEST_PATH)
+    TEST_PATHS = args[1:]
+    main(SDK_PATH, TEST_PATHS)
+
